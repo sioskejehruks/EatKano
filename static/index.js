3e8628e1173d637a320df4c3e8db7aa9ba82f699
sioskejehruks <98472326+sioskejehruks@users.noreply.github.com>
2022-02-04 11:23:43 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -441,7 +441,7 @@ const MODE_NORMAL = 1, MODE_ENDLESS = 2, MODE_PRACTICE = 3;
             SubmitResults();
         }
 
-        if (cps <= 5) return '超逊？';
+        if (cps <= 5) return '超逊';
         if (cps <= 8) return '逊';
         if (cps <= 10)  return '勇';
         if (cps <= 15) return '您';
